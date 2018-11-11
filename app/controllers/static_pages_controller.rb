8072e76f1f06a253c74ebe22a63783266f4c97be
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2018-11-11 19:23:38 +0000

    conect wp

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,16 @@ class StaticPagesController < ApplicationController
   def home
     @reviews = Review.limit(10).order('created_at DESC')
     @prefs = JpPrefecture::Prefecture.all
+    
+    conn = Faraday.new(url: ENV['WP_PATH']) do |faraday|
+    faraday.request  :url_encoded             # form-encode POST params
+    faraday.response :logger                  # log requests to STDOUT
+    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+    response = conn.get '/wp-json/wp/v2/posts?_embed'  
+
+    @parsed_articles = JSON.parse(response.body) 
   end
 
   def dbmap
