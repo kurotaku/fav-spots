470822b2ba78eed8278e64633b16c16d7fc3f4f3
root <root@localhost.localdomain>
2019-01-04 19:01:46 +0100

    remove migration

--- db/migrate/20180922120121_cange_datatype_since_of_spots.rb
+++
@@ -1,9 +0,0 @@
-class CangeDatatypeSinceOfSpots < ActiveRecord::Migration[5.1]
-  def up
-    change_column :spots, :since, :string
-  end
-
-  def down
-    change_column :spots, :since, :integer
-  end
-end
